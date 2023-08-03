a5285239f622f1d4a4c9ebcafad9e82b827cbcd6
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-08-03 08:15:39 +0300

    fixed some errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,15 +13,13 @@ def canUnlockAll(boxes):
     Returns:
         bool: True if all boxes can be opened, False otherwise.
     """
-    n = len(boxes)  # total number of boxes
-    o_boxes = set([0])  # set to store the indices of opened boxes
-    keys = boxes[0]  # keys available from the first box
+    keys = [0]  # keys available from the first box
     # Loop through the available keys and open the corresponding boxes
     for key in keys:
-        o_boxes.add(key)
         # Loop through the opened boxes and their keys to open more boxes
         for box in boxes[key]:
-            if box not in o_boxes:  # check if the box is not already opened
+            if box not in keys and box < len(boxes):
                 keys.append(box)  # add the new key to available keys
-                o_boxes.add(box)  # mark the box as opened
-    return len(o_boxes) == n  # Return True if all boxes are opened, else False
+    if len(keys) == len(boxes):
+        return True
+    return False
