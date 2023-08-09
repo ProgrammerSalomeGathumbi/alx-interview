0e2f7756acadfdbee43273f2aad113826254a796
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-08-09 12:36:05 +0300

    added comments

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,11 +11,11 @@ def minOperations(n):
     """
     if n <= 0:
         return 0
-    i = 0
-    j = 2
+    i = 0  # Initialize the count of operations to 0
+    j = 2  # Start with the smallest prime divisor
     while n > 1:
         while n % j == 0:
-            i += j
-            n //= j
-        j += 1
+            i += j  # Add the divisor to the operations count
+            n //= j  # Reduce n by the divisor
+        j += 1  # Move to the next divisor
     return i
