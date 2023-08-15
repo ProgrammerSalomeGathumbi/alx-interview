1ded16c2b3a96ad8e7c43d1180ea548a0ef249ae
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-08-15 18:29:52 +0300

    modified 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,37 +5,37 @@
 import sys
 
 
-def print_stats(total_size, sts_codes):
+def print_stats(total_size, status):
     """
     reads stdin line by line and computes metrics
     """
     print("File size: {}".format(total_size))
-    for sts in sorted(sts_codes.keys()):
-        if sts_codes[sts] != 0:
-            print("{}: {}".format(sts, sts_codes[sts]))
+    for sts in sorted(status.keys()):
+        if status[sts] != 0:
+            print("{}: {}".format(sts, status[sts]))
 
 
-sts_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
+codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
              404: 0, 405: 0, 500: 0}
-line_count = 0
-total_size = 0
+count = 0
+size = 0
 try:
     for line in sys.stdin:
-        if line_count != 0 and line_count % 10 == 0:
-            print_stats(total_size, sts_codes)
+        if count != 0 and count % 10 == 0:
+            print_stats(size, codes)
         parts = line.split()
-        line_count += 1
+        count += 1
         try:
-            total_size += int(parts[-1])
+            size += int(parts[-1])
         except ValueError:
             pass
         try:
-            sts_code = int(parts[-2])
-            if sts_code in sts_codes:
-                sts_codes[sts_code] += 1
+            code = int(parts[-2])
+            if code in codes:
+                codes[code] += 1
         except ValueError:
             pass
 
 except KeyboardInterrupt:
-    print_stats(total_size, sts_codes)
+    print_stats(size, codes)
     raise
