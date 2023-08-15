38c656a26e5b3d519f3ec3204989cf22cce6941e
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-08-15 17:29:10 +0300

    fixed naming error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,36 +5,37 @@
 import sys
 
 
-def print_stats(total_file_size, sts_code_counts):
+def print_stats(total_size, status_counts):
     """
     reads stdin line by line and computes metrics
     """
-    print("File size: {}".format(total_file_size))
-    for sts_code in sorted(sts_code_counts.keys()):
-        count = sts_code_counts[sts_code]
-        if count != 0:
-            print("{:d}: {:d}".format(sts_code, count))
+    print("File size: {}".format(total_size))
+    for sts in sorted(status_counts.keys()):
+        if status_counts[sts] != 0:
+            print("{}: {}".format(sts, status_counts[sts]))
 
 
-total_file_size = 0
-sts_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
-              404: 0, 405: 0, 500: 0}
+sts_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
+             404: 0, 405: 0, 500: 0}
 line_count = 0
+total_size = 0
 try:
     for line in sys.stdin:
         if line_count != 0 and line_count % 10 == 0:
-            print_stats(sts_counts, total_file_size)
+            print_stats(total_size, sts_codes)
         parts = line.split()
-        line_count = 0
+        line_count += 1
         try:
-            total_file_size += int(parts[-1])
+            total_size += int(parts[-1])
         except ValueError:
             pass
         try:
-            if parts[-2] in sts_counts:
-                sts_counts[parts[-2]] += 1
+            sts_code = int(parts[-2])
+            if sts_code in sts_codes:
+                sts_codes[sts_code] += 1
         except ValueError:
             pass
+
 except KeyboardInterrupt:
-    print_stats(total_file_size, sts_counts)
+    print_stats(total_size, sts_codes)
     raise
