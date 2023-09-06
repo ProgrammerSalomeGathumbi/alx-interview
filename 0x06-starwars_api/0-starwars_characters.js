99879cab5a0abddaf5de774b68a9d8226731f7de
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-09-06 13:25:15 +0300

    modified code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,29 +4,40 @@ const request = require('request');
 const movieID = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  if (response.statusCode === 200) {
-    const film = JSON.parse(body);
-    const characters = film.characters;
-    function getCharacterData (characterUrl) {
-      request(characterUrl, (error, response, characterBody) => {
-        if (error) {
-          console.log(error);
-          return;
+async function main () {
+  try {
+    request(url, async (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      if (response.statusCode === 200) {
+        const film = JSON.parse(body);
+        const characters = film.characters;
+        for (const characterUrl of characters) {
+          try {
+            const characterData = await new Promise((resolve, reject) => {
+              request(characterUrl, (error, response, characterBody) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  const character = JSON.parse(characterBody);
+                  resolve(character.name);
+                }
+              });
+            });
+            console.log(characterData);
+          } catch (characterError) {
+            console.error('Character Error:', characterError);
+          }
         }
-        const characterData = JSON.parse(characterBody);
-        console.log(characterData.name);
-      });
-    }
-
-    for (const characterUrl of characters) {
-      getCharacterData(characterUrl);
-    }
-  } else {
-    console.log('Error code: ' + response.statusCode);
+      } else {
+        console.log('Error code: ' + response.statusCode);
+      }
+    });
+  } catch (error) {
+    console.error('Error:', error);
   }
-});
+}
+
+main();
