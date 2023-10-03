0b10c70856184864ba58ce2a7b730d037315d6c2
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-10-03 20:27:55 +0300

    refactoring code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,31 +8,30 @@ def isPrimeNumber(n):
     """
     generates a list of prime numbers
     """
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False
-    p = 2
-    while p * p <= n:
-        if primes[p]:
-            for x in range(p * p, n + 1, p):
-                primes[x] = False
-        p += 1
-    return [x for x in range(n + 1) if primes[x]]
+    primes = []
+    primeNos = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if primeNos[prime]:
+            primeNos.append(prime)
+            for x in range(prime, n + 1, prime):
+                primeNos[x] = False
+    return primes
 
 
 def isWinner(x, nums):
     """
     Return: name of the player that won the most rounds
     """
-    primes = isPrimeNumber(max(max(nums), 1))
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
     Maria = 0
     Ben = 0
-    for n in nums:
-        if n == 1 or (n - 1) % 4 == 0:
+    for n in range(x):
+        primes = isPrimeNumber(nums[n])
+        if len(primes) == 0:
             Ben += 1
-        elif n in primes:
-            Maria += 1
         else:
-            Ben += 1
+            Maria += 1
     if Maria > Ben:
         return "Maria"
     elif Ben > Maria:
